e09cec18a5f096fe3c4d0c0f4bc71598d6d89fd1
Felix Gutierrez <felixvidalgu@gmail.com>
2024-12-15 19:20:30 -0300

    adding s3 function to utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from .data_generator import generate_financial_dataset
 from .data_generator import generate_customers
 from .data_generator import generate_datasets
-#from .data_generator import
+from .load_to_s3 import upload_files
 #from .data_generator import
 #from .data_generator import
